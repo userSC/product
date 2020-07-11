af1ef2d83cbb2d207c5164dd90cb1bd982858394
summer <summerchien003@gmail.com>
2020-07-11 15:53:32 +0800

    add for loop for poducts

---
+++ products.py
@@ -0,0 +1,15 @@
+##記帳程式
+products = []
+while True:
+	name = input('請輸入商品名稱: ')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格: ')
+	p =[name,price]
+	products.append(p)
+print(products)
+
+for p in products:
+	print(p[0],'的價格為: ',p[1])
+
+
